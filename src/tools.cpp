98ab6e55b4b2d6cd03f931ce81b6773e26c4bcbd
Waleed Mansoor <waleed.mansoor@gmail.com>
2017-06-04 17:29:11 +0900

    Removed a few comments

--- src/tools.cpp
+++ src/tools.cpp
@@ -12,19 +12,10 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+
 	VectorXd rmse(4);
 	rmse.fill(0);
 
-	// TODO: YOUR CODE HERE
-
-	// check the validity of the following inputs:
-	//  * the estimation vector size should not be zero
-	//  * the estimation vector size should equal ground truth vector size
-	// ... your code here
 
 	if (estimations.size() == 0
 	   || estimations.size() != ground_truth.size() ){
--- src/ukf.cpp
+++ src/ukf.cpp
@@ -29,10 +29,10 @@ UKF::UKF() {
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 0.2; // TODO: Come back to this later
+  std_a_ = 0.2; // These values work fine for me
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 0.2; // TODO: Come back to this later
+  std_yawdd_ = 0.2; // These values work fine for me
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
@@ -49,13 +49,7 @@ UKF::UKF() {
   // Radar measurement noise standard deviation radius change in m/s
   std_radrd_ = 0.3;
 
-  /**
-  TODO:
 
-  Complete the initialization. See ukf.h for other member properties.
-
-  Hint: one or more values initialized above might be wildly off...
-  */
   ///* State dimension
   time_us_ = 0;
   is_initialized_ = false;
@@ -67,12 +61,17 @@ UKF::UKF() {
   lambda_ = 3 -n_aug_;
   NIS_laser_ = NIS_radar_ = 0;
 
+  //
+  // Going with identity matrix, convergence is ok
+  //
+
   P_ << 1, 0, 0, 0, 0,
 		0, 1, 0, 0, 0,
 		0, 0, 1, 0, 0,
 		0, 0, 0, 1, 0,
 		0, 0, 0, 0, 1;
 
+  // Set up the weights
   int num_sig_pts = 2 * n_aug_ + 1;
   weights_ = VectorXd(num_sig_pts);
   for (int i=0; i < num_sig_pts; i++){
@@ -92,12 +91,7 @@ UKF::~UKF() {}
  * either radar or laser.
  */
 void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
-  /**
-  TODO:
 
-  Complete this function! Make sure you switch between lidar and radar
-  measurements.
-  */
 
 	if (!is_initialized_) {
 		// first measurement
@@ -146,12 +140,6 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
  * measurement and this one.
  */
 void UKF::Prediction(double delta_t) {
-  /**
-  TODO:
-
-  Complete this function! Estimate the object's location. Modify the state
-  vector, x_. Predict sigma points, the state, and the state covariance matrix.
-  */
 
 	int num_sig_pts = 2 * n_aug_ + 1;
 
@@ -263,14 +251,7 @@ void UKF::Prediction(double delta_t) {
  * @param {MeasurementPackage} meas_package
  */
 void UKF::UpdateLidar(MeasurementPackage meas_package) {
-  /**
-  TODO:
 
-  Complete this function! Use lidar data to update the belief about the object's
-  position. Modify the state vector, x_, and covariance, P_.
-
-  You'll also need to calculate the lidar NIS.
-  */
 // First change prediction to measurement space
 
 	int n_z = 2; // Number of radar measurement dimensions
@@ -363,14 +344,6 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
  * @param {MeasurementPackage} meas_package
  */
 void UKF::UpdateRadar(MeasurementPackage meas_package) {
-  /**
-  TODO:
-
-  Complete this function! Use radar data to update the belief about the object's
-  position. Modify the state vector, x_, and covariance, P_.
-
-  You'll also need to calculate the radar NIS.
-  */
 
 	// First change prediction to measurement space
 
